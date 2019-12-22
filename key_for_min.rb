1983ec21bd2b54b05bb0fb70c406d8e0b6dcec76
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 21:03:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,10 @@ require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
-  x = 1
-  hash.each do |i,price| 
-  if price < x 
-    x = price
-    price
-end
-end
-binding.pry
-hash["#{x}"]
+def key_for_min_value(name_hash)
+  name_hash.collect do |item, price|
+    new_arr << price
+
+
+
 end
